466d8c8913daa63b0ce9b10fb48f0e147b2b3ae2
Min Khant Kyaw <157019502+minkhant1020@users.noreply.github.com>
2024-02-02 09:28:37 +0630

    change file

--- app/src/main/java/mkkcom/example/apptest/MainActivity.kt
+++ app/src/main/java/mkkcom/example/apptest/MainActivity.kt
@@ -2,6 +2,7 @@ package mkkcom.example.apptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.inputmethod.InputBinding
 import mkkcom.example.apptest.databinding.ActivityMainBinding
@@ -9,9 +10,40 @@ import mkkcom.example.apptest.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("TAG","onCreate")
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.tvGreeting.text="Mingalarpar"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG","onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("TAG","onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG","onDestroy")
     }
 }
